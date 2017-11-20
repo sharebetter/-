99cbbacb468e261aa4ab46be7166918ff1d597aa
sharebetter <4041812@qq.com>
2017-11-20 09:14:12 +0800

    Update delete.php

--- admin/content/delete.php
+++ admin/content/delete.php
@@ -1,10 +1,14 @@
 <?php
 include '../../public/db.php';
    $id=$_GET['id'];
-   $sql = "delete topic,content from topic,content where topic.id={$id} and topic.id=content.topic_id";
+   $sql = "delete from topic where id={$id}";   
    $rst = $pdo->query($sql);
    if($rst){
-   	 header("location:index.php");
+   	$sql2 = "delete from content where topic_id={$id}";
+   	$rst2 = $pdo->query($sql2);
+   	if($rst2){
+      header("location:index.php");
+   	}   	 
    	 // echo "string";
    }
 ?>
